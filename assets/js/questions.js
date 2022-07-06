949d6a3c94d337786f926760140435f27a24c2a5
AFaidley <alviva.faidley@gmail.com>
2022-07-05 18:38:12 -0600

    Added pseudo code to js files

---
+++ assets/js/questions.js
@@ -0,0 +1,1 @@
+// Var questions and answer options
---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Vars for buttons and elements, global scope
+
+// Function to start game
+
+// Function for questions
+
+// Function for timer
+
+// Function for highscores
+
+// End of game 
