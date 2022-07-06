8afdd2d57a4721dcf48c9f2c3534c8bac5ed1cb6
AFaidley <alviva.faidley@gmail.com>
2022-07-06 14:34:56 -0600

    Added js questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,1 +1,57 @@
 // Var questions and answer options
+var questions = [
+    {
+        question: "Which of the following is NOT a Javascript data type?",
+        options: [
+            "Boolean",
+            "Number",
+            "Undefined",
+            "Straight"
+        ],
+        answer: "Straight"
+    },
+
+    {
+        question: "Which of the following is NOT a loop structure in Javascript?",
+        options: [
+            "For",
+            "While",
+            "Hoola-Loop",
+            "Do-While"
+        ],
+        answer: "Hoola-Loop"
+    },
+
+    {
+        question: "What is the meaning of NaN?",
+        options: [
+            "Not available now",
+            "Never also no",
+            "Not a number",
+            "Never a number"     
+        ],
+        answer: "Not a number"
+    },
+
+    {
+        question: "What is the data type of variables in Javascript?",
+        options: [
+            "Boolean",
+            "Object",
+            "String",
+            "Method"
+        ],
+        answer: "Object"
+
+    },
+
+    {
+        question: "How do you define a variable called varName?",
+        options: [
+            "var varName = 'varName'",
+            "var = 'varName'",
+            "varName = 'varName'",
+            "var > varName"
+        ],
+    }
+];
