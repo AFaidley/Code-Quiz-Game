b303c684a311e7de5fa2d6d9f98422f247586892
AFaidley <alviva.faidley@gmail.com>
2022-07-09 17:33:28 -0600

    Added code to getQuestions func as well as created new function for onclick func to show next questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var highscores = document.getElementById("highscore");
 var submitBtn = document.getElementById("submit");
 var initials = document.getElementById("initials");
 var timeChange = document.getElementById("time");
-var timeLeft = 5000;
+var timeLeft = 60;
 var start = document.getElementById("main");
 var end = document.getElementById("end");
 var qBox = document.getElementById("qbox");
@@ -14,30 +14,43 @@ var questionI = 0;
 
 // Function to start game
 function startGame() {
-    start.setAttribute("class", "hidden");
-    getQuestions();
+  start.setAttribute("class", "hidden");
+  getQuestions();
 }
 // Function for questions
 function getQuestions() {
-    var currentQ = questions[questionI];
-    qTitle.textContent = currentQ.question;
-    qContent.textContent = "";
-
-    currentQ.options.forEach(function(choice, i) {
-        var qOption = document.createElement("button");
-        qOption.setAttribute("class", choice);
-        qOption.setAttribute("value", choice);
-        qOption.textContent = i + 1 + "." + choice;
-        // qOption.onClick = qClick;
-        qContent.appendChild(qOption)
-    });
-
-
+  var currentQ = questions[questionI];
+  qTitle.textContent = currentQ.question;
+  qContent.textContent = "";
+// Displays the question
+  currentQ.options.forEach(function (choice, i) {
+    var qOption = document.createElement("button");
+    qOption.setAttribute("class", "choice");
+    qOption.setAttribute("value", choice);
+    qOption.textContent = i + 1 + ") " + choice;
+    qOption.onclick = qClick;
+    qContent.appendChild(qOption);
+  });
 }
+// Function to determine whether the correct answer was selected
+function qClick() {
+  if (this.value !== questions[questionI].answer) {
+    timeLeft -= 10;
+  }
+  // Goes to the next question
+  questionI++;
+// Ends game when all questions have been answered or gets new question if not
+  if (questionI === questions.length) {
+    endGame();
+  } else {
+    getQuestions();
+  }
+}
+
 // Function for timer
 
 function countdown() {
-  var timeInterval = setInterval(function () {
+  var timeInterval = setInterval(() => {
     if (timeLeft > 1 || timeLeft === 1) {
       timeChange.textContent = timeLeft;
       timeLeft--;
@@ -50,11 +63,11 @@ function countdown() {
 }
 
 // Function for highscores
-function highScore() {
-}
-// End of game
+function highScore() {}
+// End of game, hides questions and un-hides the end screen (initials and end message)
 function endGame() {
   end.setAttribute("class", "");
+  qBox.setAttribute("class", "hidden");
 }
 // Call functions
 startBtn.onclick = () => {
